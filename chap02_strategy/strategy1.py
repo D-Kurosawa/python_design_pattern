668779840433d48fb0255bf30ee34f795a5e25aa
Daisuke Kurosawa <dai-kuro@sings.jp>
2019-06-02 17:08:10 +0900

    [add] strategy1.py

---
+++ chap02_strategy/strategy1.py
@@ -0,0 +1,44 @@
+class Report(object):
+    def __init__(self, title, text, formatter):
+        self.title = title
+        self.text = text
+        self.formatter = formatter
+
+    def output_report(self):
+        self.formatter.output_report(self.title, self.text)
+
+
+class Formatter(object):
+    def output_report(self, title, text):
+        assert False
+
+
+class HTMLFormatter(Formatter):
+    def output_report(self, title, text):
+        print("<html>")
+        print("<head>")
+        print("<title>%s</title>" % (title,))
+        print("</head>")
+        print("<body>")
+        for line in text:
+            print("<p>%s</p>" % (line,))
+        print("</body>")
+        print("</html>")
+
+
+class PlainTextFormatter(Formatter):
+    def output_report(self, title, text):
+        print("***%s***" % (title,))
+        for line in text:
+            print(line)
+
+
+if __name__ == "__main__":
+    report = Report(u"月次報告", [u"順調！", u"最高です！"],
+                    PlainTextFormatter())
+    report.output_report()
+    print("-" * 70)
+    report = Report(u"月次報告", [u"順調！", u"最高です！"],
+                    HTMLFormatter())
+    report.output_report()
+    print("-" * 70)
