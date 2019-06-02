6ffa1af14cb9291ca280fc89b4fbc781e5190fe2
Daisuke Kurosawa <dai-kuro@sings.jp>
2019-06-02 15:18:56 +0900

    [rename] format -> output_format

--- chap01_template_method/template_method1.py
+++ chap01_template_method/template_method1.py
@@ -3,10 +3,10 @@ class Report:
         self.title = "月次報告"
         self.text = ["順調", "最高"]
 
-    def output_report(self, format):
-        if format == "plain":
+    def output_report(self, output_format):
+        if output_format == "plain":
             print(f"*** {self.title} ***")
-        elif format == "html":
+        elif output_format == "html":
             print("<html>")
             print("<head>")
             print(f"<title>{self.title}</title>")
@@ -14,12 +14,12 @@ class Report:
             print("<body>")
 
         for line in self.text:
-            if format == "plain":
+            if output_format == "plain":
                 print(line)
             else:
                 print(f"<p>{line}</p>")
 
-        if format == "html":
+        if output_format == "html":
             print("</body>")
             print("</html>")
 
