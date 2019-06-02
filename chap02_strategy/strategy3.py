c8c8aae5ac4ea467d7e53c94bb9ee3692ed5eacd
Daisuke Kurosawa <dai-kuro@sings.jp>
2019-06-02 18:43:27 +0900

    [add] docstrings

--- chap02_strategy/strategy3.py
+++ chap02_strategy/strategy3.py
@@ -1,3 +1,16 @@
+"""
+Report.output_reportでformatterに渡されているのは、titleとtext の2つだけですが、
+今後渡すべき要素が増える可能性もあります。例えば日付や提出者名など。
+
+そこで、一々要素を選んで渡すのではなく、Reportオブジェクトそのものを渡してしまう方法もあります。
+
+しかし、こうするとformatterとReportの結合度が増してしまいます。
+
+Strategyパターンを使う際は、ストラテジの範囲と渡すべきデータを見極める必要があり、
+それを誤ると、第2第3のStrategyの適用を妨げることにもなります。
+"""
+
+
 class Report:
     def __init__(self, title, text, formatter):
         self.title = title
@@ -5,7 +18,9 @@ class Report:
         self.formatter = formatter
 
     def output_report(self):
-        # self.formatter(self.title, self.text)
+        """
+        self.formatter(self.title, self.text) -> self.formatter(self)
+        """
         self.formatter(self)
 
 
